87a02b60e837949a2347076d716f790b064cb060
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-10 17:34:40 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+#+++++++++++"IMPORT"++++++++++++++++++
+import requests
+import tkinter
+import os
+import urllib.request
+import random
+
+#++++++Try+++++++++++
+try:
+  import dehook as dk
+  import pcolor
+except:
+  print("IMPORT ERROR")
+  
+#++++++++++++++++++++
+
+import threading
+import random
+import time
+
+#++++++++++++++++++++++++++++++++++++
+
+def dos():
+  r = urllib.request.urlopen('https://api').read().decode('utf-8')
+  f = open("bd.txt", 'a', encoding='utf-8', newline='\n')
+  f.write(r)
+  f.close()
+  
+  for proxy in res: proxies.append("https://" + proxy)
+    res = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=5000").text.splitlines()
+    print(res)
+  t=random.choice(res)
+  print(f"t={t}")
+  proxies = {
+    'https': f"https://{t}"
+  }
+  try:
+    a=requests.get("m.kuku.lu",proxies=proxies,timeout=(3.0, 7.5))
+    status=a.status_code
+  except:
+    print("")
+  
+dos()
+
+
